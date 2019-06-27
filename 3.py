3feead92fc3dbbd02f11cf94d5bc73da2acd5a2f
antoprinciya <51980780+antoprinciya@users.noreply.github.com>
2019-06-27 11:34:41 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,9 @@
+s=input()
+d=['a','e','i','o','u','A','E','I','O','U']
+k=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
+if s in d:
+  print("Vowel")
+elif s in k:
+  print("Consonant")
+else:
+  print("invalid")
