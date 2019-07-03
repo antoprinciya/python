c62c776c0b83e39aeb81e6da759271ae169bf6fc
antoprinciya <51980780+antoprinciya@users.noreply.github.com>
2019-07-03 11:52:55 +0530

    Delete 16.py

--- 16.py
+++
@@ -1,8 +0,0 @@
-p,r=map(int,input().split())
-for i in range(p+1,r,1):
-    if(i>1):
-        for v in range(2,i):
-            if(i%v==0):
-                break
-        else:
-            print(i,end=" ")
